215de81f887597754196ba86aa8eec509394f35b
Brian Miller <brian@theinkshop.ie>
2020-07-06 20:19:39 +0100

    added add to bag functionality

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
     """ A view to return the shopping basket page """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ A view to add products to a shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('basket', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['basket'] = bag
+    print(request.session['basket'])
+    return redirect(redirect_url)
