746c059d815c16ecdaba96f8e50bc6c3e7c83165
Brian Miller <brian@theinkshop.ie>
2020-07-11 16:01:14 +0100

    added toasts templates

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_basket(request):
@@ -10,6 +12,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ A view to add products to a shopping basket """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -32,6 +35,7 @@ def add_to_basket(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['basket'] = bag
     return redirect(redirect_url)
