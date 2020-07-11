0abe11162781bac498b067acbe65d74773fffcec
Brian Miller <brian@theinkshop.ie>
2020-07-11 14:00:03 +0100

    added update and remove to/from shopping bag views and added cal_subtotal template filter

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -34,5 +34,55 @@ def add_to_basket(request, item_id):
             bag[item_id] = quantity
 
     request.session['basket'] = bag
-    print(request.session['basket'])
     return redirect(redirect_url)
+
+
+def update_basket(request, item_id):
+    """ A view to update product quantities in the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    bag = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['basket'] = bag
+    return redirect(reverse('view_basket'))
+
+def remove_item_from_basket(request, item_id):
+    """ A view to update product quantities in the shopping basket """
+
+    size = None
+    try:  
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+
+        bag = request.session.get('basket', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['basket'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
