ac8f98fd023c11b77106dce09e9e93fc315770d5
Brian Miller <brian@theinkshop.ie>
2020-07-14 20:18:01 +0100

    addedbasic checkout functionality, test card success with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -3,24 +3,41 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
     """ A view to show the checkout """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('basket', {})
     if not bag:
         messages.error(request, "You haven't added anything to your basket!")
         return redirect(reverse('products'))
     
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY, 
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Did you forget to set your stripe public key in your environment variables?')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
 
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'clients secret key',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
