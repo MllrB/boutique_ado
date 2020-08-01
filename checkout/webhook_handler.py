5e77c765e6f5930b959a70aa38bbf7159737ed66
Brian Miller <brian@theinkshop.ie>
2020-08-01 14:36:23 +0100

    testing stripe webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -54,8 +54,8 @@ class StripeWH_Handler:
         
         intent = event.data.object
         pid = intent.id
-        bag = intent.metadata.basket
-        save_info = intent.metadata.save_info
+        #bag = intent.metadata.basket
+        #save_info = intent.metadata.save_info
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount /100, 2)
