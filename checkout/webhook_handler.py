4de4724efa1a358e20199e4ba922868782268ab7
Brian Miller <brian@theinkshop.ie>
2020-08-01 15:11:47 +0100

    removed

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -54,15 +54,8 @@ class StripeWH_Handler:
         
         intent = event.data.object
         pid = intent.id
-        if intent.metadata.bag:
-            bag = intent.metadata.bag
-        else:
-            bag = None
-        
-        if intent.metadata.save_info:
-            save_info = intent.metadata.save_info
-        else:
-            save_info = False
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
         
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
