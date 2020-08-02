b36d7a67372f969b1c3bc43748995da6961db3c4
Brian Miller <brian@theinkshop.ie>
2020-08-02 14:15:44 +0100

    mobile styling for home,product and login/register pages

--- checkout/views.py
+++ checkout/views.py
@@ -27,8 +27,8 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(request, f'Sorry, your payment cannot be \
+            processed right now. Please try again later.error: {e}')
         return HttpResponse(content=e, status=400)
 
 
