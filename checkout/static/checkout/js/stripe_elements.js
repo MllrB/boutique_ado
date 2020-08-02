abef4e08748e0b4bfc9645bef725bedc5d77c820
Brian Miller <brian@theinkshop.ie>
2020-08-02 15:38:35 +0100

    fixed payment error

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,5 @@
 var stripePublicKey = $('#stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#client_secret').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
